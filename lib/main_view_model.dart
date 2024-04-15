68e409a1ea8c04cff1eac6a36d73c914ed6052d1
sseung <sseutta@naver.com>
2024-04-15 14:36:25 +0900

    테스트

--- lib/main_view_model.dart
+++ lib/main_view_model.dart
@@ -1,32 +1,28 @@
-// import 'package:album/data/repository/movie_detail_repo.dart';
-// import 'package:album/data/repository/movie_info_repo.dart';
-// import 'package:flutter/material.dart';
-//
-// import 'data/model/movie_info.dart';
-//
-// //화면 이름에 맞춰서 viewModel class 작성
-// class MovieViewModel with ChangeNotifier {
-//   //ChangeNotifier Setstate 편하게 쓰라는 위젯
-//   final MovieRepository _movieRepository;
-//   final MovieDetailRepository _movieDetailRepository;
-//
-//   MovieViewModel(
-//       {required MovieRepository movieRepository,
-//         required MovieDetailRepository movieDetailRepository})
-//       : _movieRepository = movieRepository,
-//         _movieDetailRepository = movieDetailRepository;
-//
-//   List<MovieInfo> _movies = [];
-//
-//   List<MovieInfo> get movies => List.unmodifiable(_movies);
-//
-//   bool _isLoading = false;
-//
-//   bool get isLoading => _isLoading;
-//
-//   //일부러 Future 안씀
-//   void fetchMovies() async {
-//     _movies = await _movieRepository.getMovieInfoList();
-//     notifyListeners(); //UI 갱신 알림
-//   }
-// }
+import 'package:flutter/material.dart';
+import 'package:flutter_subway/data/model/subway.dart';
+import 'package:flutter_subway/data/repository/subway_repo.dart';
+
+//화면 이름에 맞춰서 viewModel class 작성
+class SubwayViewModel with ChangeNotifier {
+  //ChangeNotifier Setstate 편하게 쓰라는 위젯
+  final SubwayRepository _subwayRepository;
+
+  SubwayViewModel(
+      {required SubwayRepository subwayRepository,
+        })
+      : _subwayRepository = subwayRepository;
+
+  List<Subway> _subways = [];
+
+  List<Subway> get subways => List.unmodifiable(_subways);
+
+  bool _isLoading = false;
+
+  bool get isLoading => _isLoading;
+
+  //일부러 Future 안씀
+  void fetchSubways() async {
+    _subways = await _subwayRepository.getSubway();
+    notifyListeners(); //UI 갱신 알림
+  }
+}
