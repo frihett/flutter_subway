16095e981033e0060d2921ff9760f4b22845c2f8
jungeun272 <hanjungeun272@gmail.com>
2024-04-15 16:35:45 +0900

    test

--- lib/data/data_source/subway_api.dart
+++ lib/data/data_source/subway_api.dart
@@ -1,17 +1,18 @@
 import 'dart:convert';
 
+import 'package:flutter_subway/data/dto/subway_dto.dart';
 import 'package:flutter_subway/data/model/subway.dart';
 import 'package:http/http.dart' as http;
 
 
 class SubwayApi {
-  Future<List<Subway>> getSubwayList() async {
+  Future<List<SubwayDto>> getSubwayList() async {
     final response = await http.get(Uri.parse('http://swopenapi.seoul.go.kr/api/subway/sample/json/realtimeStationArrival/0/5/%EC%84%9C%EC%9A%B8'));
 
     if (response.statusCode == 200) {
       final Map<String, dynamic> jsonData = jsonDecode(response.body);
       final List<dynamic> realtimeArrivalList = jsonData['realtimeArrivalList'];
-      return realtimeArrivalList.map((json) => Subway.fromJson(json)).toList();
+      return realtimeArrivalList.map((json) => SubwayDto.fromJson(json)).toList();
     } else {
       throw Exception('Error');
     }
---
+++ lib/data/dto/subway_dto.dart
@@ -0,0 +1,132 @@
+class SubwayDto {
+  SubwayDto({
+      this.beginRow, 
+      this.endRow, 
+      this.curPage, 
+      this.pageRow, 
+      this.totalCount, 
+      this.rowNum, 
+      this.selectedCount, 
+      this.subwayId, 
+      this.subwayNm, 
+      this.updnLine, 
+      this.trainLineNm, 
+      this.subwayHeading, 
+      this.statnFid, 
+      this.statnTid, 
+      this.statnId, 
+      this.statnNm, 
+      this.trainCo, 
+      this.trnsitCo, 
+      this.ordkey, 
+      this.subwayList, 
+      this.statnList, 
+      this.btrainSttus, 
+      this.barvlDt, 
+      this.btrainNo, 
+      this.bstatnId, 
+      this.bstatnNm, 
+      this.recptnDt, 
+      this.arvlMsg2, 
+      this.arvlMsg3, 
+      this.arvlCd,});
+
+  SubwayDto.fromJson(dynamic json) {
+    beginRow = json['beginRow'];
+    endRow = json['endRow'];
+    curPage = json['curPage'];
+    pageRow = json['pageRow'];
+    totalCount = json['totalCount'];
+    rowNum = json['rowNum'];
+    selectedCount = json['selectedCount'];
+    subwayId = json['subwayId'];
+    subwayNm = json['subwayNm'];
+    updnLine = json['updnLine'];
+    trainLineNm = json['trainLineNm'];
+    subwayHeading = json['subwayHeading'];
+    statnFid = json['statnFid'];
+    statnTid = json['statnTid'];
+    statnId = json['statnId'];
+    statnNm = json['statnNm'];
+    trainCo = json['trainCo'];
+    trnsitCo = json['trnsitCo'];
+    ordkey = json['ordkey'];
+    subwayList = json['subwayList'];
+    statnList = json['statnList'];
+    btrainSttus = json['btrainSttus'];
+    barvlDt = json['barvlDt'];
+    btrainNo = json['btrainNo'];
+    bstatnId = json['bstatnId'];
+    bstatnNm = json['bstatnNm'];
+    recptnDt = json['recptnDt'];
+    arvlMsg2 = json['arvlMsg2'];
+    arvlMsg3 = json['arvlMsg3'];
+    arvlCd = json['arvlCd'];
+  }
+  dynamic beginRow;
+  dynamic endRow;
+  dynamic curPage;
+  dynamic pageRow;
+  num? totalCount;
+  num? rowNum;
+  num? selectedCount;
+  String? subwayId;
+  dynamic subwayNm;
+  String? updnLine;
+  String? trainLineNm;
+  dynamic subwayHeading;
+  String? statnFid;
+  String? statnTid;
+  String? statnId;
+  String? statnNm;
+  dynamic trainCo;
+  String? trnsitCo;
+  String? ordkey;
+  String? subwayList;
+  String? statnList;
+  String? btrainSttus;
+  String? barvlDt;
+  String? btrainNo;
+  String? bstatnId;
+  String? bstatnNm;
+  String? recptnDt;
+  String? arvlMsg2;
+  String? arvlMsg3;
+  String? arvlCd;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['beginRow'] = beginRow;
+    map['endRow'] = endRow;
+    map['curPage'] = curPage;
+    map['pageRow'] = pageRow;
+    map['totalCount'] = totalCount;
+    map['rowNum'] = rowNum;
+    map['selectedCount'] = selectedCount;
+    map['subwayId'] = subwayId;
+    map['subwayNm'] = subwayNm;
+    map['updnLine'] = updnLine;
+    map['trainLineNm'] = trainLineNm;
+    map['subwayHeading'] = subwayHeading;
+    map['statnFid'] = statnFid;
+    map['statnTid'] = statnTid;
+    map['statnId'] = statnId;
+    map['statnNm'] = statnNm;
+    map['trainCo'] = trainCo;
+    map['trnsitCo'] = trnsitCo;
+    map['ordkey'] = ordkey;
+    map['subwayList'] = subwayList;
+    map['statnList'] = statnList;
+    map['btrainSttus'] = btrainSttus;
+    map['barvlDt'] = barvlDt;
+    map['btrainNo'] = btrainNo;
+    map['bstatnId'] = bstatnId;
+    map['bstatnNm'] = bstatnNm;
+    map['recptnDt'] = recptnDt;
+    map['arvlMsg2'] = arvlMsg2;
+    map['arvlMsg3'] = arvlMsg3;
+    map['arvlCd'] = arvlCd;
+    return map;
+  }
+
+}
